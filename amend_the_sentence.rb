e4e3d5c588b8964f5ecce933a1ab347a22b8abfc
Constantine Lebedev <koss.lebedev@gmail.com>
2017-07-29 21:45:59 +0500

    Amend the sentence RegEx

--- amend_the_sentence.rb
+++ amend_the_sentence.rb
@@ -16,6 +16,10 @@ def amend_the_sentence(str)
   str
 end
 
+def amend_the_sentence_regex(str)
+  str.gsub(/[A-Z]{1}/) { |match| ' ' + match[0].downcase }.strip
+end
+
 
 
 require 'minitest/autorun'
@@ -23,5 +27,9 @@ require 'minitest/autorun'
 describe 'solution' do
   it 'amends the sentence' do
     assert_equal('this is my sentence', amend_the_sentence('ThisIsMySentence'))
+  end
+
+  it 'amends the sentence using RegEx' do
+    assert_equal('this is my sentence', amend_the_sentence_regex('ThisIsMySentence'))
   end
 end
